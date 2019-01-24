060ca6b523625bec7e76dcb998d6c3fc87a02565
Joshua Colicchio <jacolicchio8554@eagle.fgcu.edu>
2019-01-24 15:46:09 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+// Joshua Colicchio
+// Program integrating skills learned in COP 2006
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		 * When using a Scanner with strings
+		 * you need to use .nextLine() to 
+		 * move to the new line / clear the buffer
+		 * when going from numbers to strings
+		 */
+	}
+
+}
