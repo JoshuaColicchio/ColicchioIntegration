0ac0c76c17d48ea0c170a2f3ea9b384f57616956
JoshuaColicchio <47011102+JoshuaColicchio@users.noreply.github.com>
2019-02-12 10:50:55 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,106 +1,96 @@
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.image.BufferStrategy;
-import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferInt;
-import java.util.ArrayList;
-import javax.swing.JFrame;
+import java.util.Scanner;
 
-public class Game extends JFrame implements Runnable{
-    public Camera camera;
-    public Screen screen;
-    
-	private static final long serialVersionUID = 1L;
-	public int mapWidth = 15;
-	public int mapHeight = 15;
-	private Thread thread;
-	private boolean running;
-	private BufferedImage image;
-	public int[] pixels;
-	public static int[][] map = 
-	{
-		{1,1,1,1,1,1,1,1,2,2,2,2,2,2,2},
-		{1,0,0,0,0,0,0,0,2,0,0,0,0,0,2},
-		{1,0,3,3,3,3,3,0,0,0,0,0,0,0,2},
-		{1,0,3,0,0,0,3,0,2,0,0,0,0,0,2},
-		{1,0,3,0,0,0,3,0,2,2,2,0,2,2,2},
-		{1,0,3,0,0,0,3,0,2,0,0,0,0,0,2},
-		{1,0,3,3,0,3,3,0,2,0,0,0,0,0,2},
-		{1,0,0,0,0,0,0,0,2,0,0,0,0,0,2},
-		{1,1,1,1,1,1,1,1,4,4,4,0,4,4,4},
-		{1,0,0,0,0,0,1,4,0,0,0,0,0,0,4},
-		{1,0,0,0,0,0,1,4,0,0,0,0,0,0,4},
-		{1,0,0,2,0,0,1,4,0,3,3,3,3,0,4},
-		{1,0,0,0,0,0,1,4,0,3,3,3,3,0,4},
-		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,4},
-		{1,1,1,1,1,1,1,4,4,4,4,4,4,4,4}
-	};
-		
-	public static void main(String [] args) {
-    	Game game = new Game();
-    }	
-		
-	public Game() {
-	    camera = new Camera(4.5, 4.5, 1, 0, 0, -.66);
-	    screen = new Screen(map, mapWidth, mapHeight, textures, 640, 480);
-        addKeyListener(camera);
-		thread = new Thread(this);
-		image = new BufferedImage(640, 480, BufferedImage.TYPE_INT_RGB);
-		pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
-		setSize(640, 480);
-		setResizable(false);
-		setTitle("3D Engine");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBackground(Color.black);
-		setLocationRelativeTo(null);
-		setVisible(true);
-		start();
+// Joshua Colicchio
+// Program integrating skills learned in COP 2006
+
+/*
+ * A variable in Java is like a box that you can store things in. The type of thing you can store
+ * in the box depends on the type of box that you create. Realistically, a variable is a name for a section of memory.
+ *
+ * Scope in Java is the level of access a variable has / where a variable 'exists'.
+ * For example, if you declare a variable inside of a method, that variable is scoped only in that method, not outside of it.
+ */
+public class Main {
+  
+	public static void main(String[] args) {
+		System.out.println("Welcome to my integration project!");
+		promptUser();
 	}
 	
-	private synchronized void start() {
-	   running = true;
-	   thread.start();
-    }
-    
-    public synchronized void stop() {
-    	running = false;
-    	try {
-    		thread.join();
-    	} catch(InterruptedException e) {
-    		e.printStackTrace();
-    	}
-    }
-    
-    public void render() {
-    	BufferStrategy bs = getBufferStrategy();
-    	if(bs == null) {
-    		createBufferStrategy(3);
-    		return;
-    	}
-    	Graphics g = bs.getDrawGraphics();
-    	g.drawImage(image, 0, 0, image.getWidth(), image.getHeight(), null);
-    	bs.show();
-    }
-    
-    public void run() {
-    	long lastTime = System.nanoTime();
-    	final double ns = 1000000000.0 / 60.0;//60 times per second
-    	double delta = 0;
-    	requestFocus();
-    	while(running) {
-    	    camera.update(map);
-    	    screen.update(camera,pixels);
-    		long now = System.nanoTime();
-    		delta = delta + ((now-lastTime) / ns);
-    		lastTime = now;
-    		while (delta >= 1)//Make sure update is only happening 60 times a second
-    		{
-    			//handles all of the logic restricted time
-    			
-    			delta--;
-    		}
-    		render();//displays to the screen unrestricted time
-    	}
-    }
+	public static void promptUser() {
+		// The "final" keyword makes it so the program cannot change the value of the variable.
+		// IE: If I typed PromptMessage = "some new message", the compiler would throw an error.
+		final String PROMPT_MSG = "Type a data type to learn more about it. Type \"Options\" for available data types";
+		System.out.println(PROMPT_MSG);
+		Scanner iostream = new Scanner(System.in);
+		String UserInput = iostream.nextLine();
+		// The String.trim method removes any whitespace from the beginning and
+		// end of the string.
+		displayMenu(UserInput.trim());
+	}
+	
+	private static void displayMenu(String response) {
+		/*
+		 *  The String.toLowerCase method converts a given string to
+		 *  its lower case counterpart.
+		 *  IE - "Hello World".toLowerCase() will return
+		 *  "hello world"
+	 	 */
+		switch(response.toLowerCase()) {
+			case "string": displayData(0); break;
+			case "int": displayData(1); break;
+			case "double": displayData(2); break;
+			case "float": displayData(3); break;
+			case "boolean": displayData(4); break;
+			case "options": displayData(999); break;
+			default: 
+				// The String.toUpperCase method converts a given string into its upper case counterpart.
+				// IE - "Hello World".toUpperCase() will return "HELLO WORLD"
+				System.out.println("I have no idea what a \"" + response.toUpperCase() + "\" is..."); 
+				promptUser(); 
+				break;
+		}
+	}
+	
+	private static void displayData(long input) {
+		// By casting the int i as a long, I can assign the value of i to the 
+		// long L, and use L for further operations.
+		int switchInput = (int)input;
+		switch (switchInput) {
+			case 0:  // string
+				System.out.println("A String is a collection of chars."); 
+				break;
+			case 1: // int
+				System.out.println("An int is a whole number."); 
+				break; 
+			case 2: // double
+				System.out.println("A double is a 64-bit number with a fractional element."); 
+				break; 
+			case 3: // float
+				System.out.println("A float is a 32-bit number with a fractional element."); 
+				break; 
+			case 4: // bool
+				System.out.println("A boolean is a true / false statement."); 
+				break; 
+			case 999: // options
+				System.out.println("Available options:\n\tString\n\tInt\n\tDouble\n\tFloat\n\tBoolean\n\tOptions"); 
+				break; 
+		}
+		promptUser();
+	}
 }
 
+/*
+ * A list and description of Java built-in data types within the user interface or in comments
+	Use the four main types of variables (boolean, int, double, String) with appropriate names and define (in English) variable and scope
+	Use final and describe what it means in a comment
+	
+ * When using a Scanner with strings
+ * you need to use .nextLine() to 
+ * move to the new line / clear the buffer
+ * when going from numbers to strings
+ * 
+ * A CALL contains an ARGUMENT. A HEADER contains a Parameter
+ * 
+ * A Method is a group of statements 
+ */
